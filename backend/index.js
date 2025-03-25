c368713a4cab81fd7e37517c276119f770474722
davu111 <vuvanvuonglqm@gmail.com>
2025-03-25 10:40:48 +0700

    Install Backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+}); 
